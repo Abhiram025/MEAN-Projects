2de8324131dff3beee6a3fb44c71975a5a56e96c
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-11 17:21:28 +0530

    script.js

--- HTML-CSS-JS/Single-Page/Project-A/script.js
+++ HTML-CSS-JS/Single-Page/Project-A/script.js
@@ -4,20 +4,30 @@ const getTemp=document.getElementById("get-temp")
 const humidPercent=document.getElementById("humid-percent")
 const windPercent=document.getElementById("wind-percent")
 
-async function weatherReport() {
+const fahrenheit=document.getElementById("fahrenheit").checked
+const celsius=document.getElementById("celsius").checked
+const kelvin=document.getElementById("kelvin").checked
+
+getTemp.addEventListener('click', ()=> { 
     const cityName=document.getElementById("city-name").value
     const apiKey="cf94d7d3e74b6f8bdbb77d49673e3a2a"
-    const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric"
-    const apiDetails=`&q=${cityName}&appid=${apiKey}`
+    const apiUrl="https://api.openweathermap.org/data/2.5/weather?"
+    const apiDetails=`q=${cityName}&appid=${apiKey}&units=${scale}`
+    const scale=fahrenheit?"standard":celsius?"metric":"imperial"
+    const unit=fahrenheit?"°F":celsius?"°c":"°k"
 
-    const ob1=await fetch(apiUrl+apiDetails)
-    const weatherObj=await ob1.json()
-    temp.textContent=`${weatherObj.main.temp}°c`
-    city.textContent=weatherObj.name
-    windPercent.textContent=`${weatherObj.wind.speed}KMPH`
-    humidPercent.textContent=`${weatherObj.main.humidity}%`
-}
-
-getTemp.addEventListener('click', ()=> {
-    weatherReport() 
+    if(!cityName) {
+        alert("Enter the city name")
+        cityName.focus()
+    }
+    else {
+        fetch(apiUrl+apiDetails)
+        .then(response=>response.json())
+        .then(data=>{
+            temp.textContent=`${data.main.temp}${unit}`
+            city.textContent=data.name
+            windPercent.textContent=`${data.wind.speed}Kmph`
+            humidPercent.textContent=`${data.main.humidity}%`
+        })
+    }    
 })
