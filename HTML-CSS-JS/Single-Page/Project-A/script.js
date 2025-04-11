3d0d1303673929d114cefc7c714e4da36d0d12f4
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-11 15:33:14 +0530

    script.js

--- HTML-CSS-JS/Single-Page/Project-A/script.js
+++ HTML-CSS-JS/Single-Page/Project-A/script.js
@@ -1,15 +1,15 @@
-const cityName=document.getElementById("city-name").value
 const temp=document.getElementById("temp")
 const city=document.getElementById("city")
 const getTemp=document.getElementById("get-temp")
 const humidPercent=document.getElementById("humid-percent")
 const windPercent=document.getElementById("wind-percent")
 
-const apiKey="cf94d7d3e74b6f8bdbb77d49673e3a2a"
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric"
-const apiDetails=`&q=${cityName}&appid=${apiKey}`
-
 async function weatherReport() {
+    const cityName=document.getElementById("city-name").value
+    const apiKey="cf94d7d3e74b6f8bdbb77d49673e3a2a"
+    const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric"
+    const apiDetails=`&q=${cityName}&appid=${apiKey}`
+
     const ob1=await fetch(apiUrl+apiDetails)
     const weatherObj=await ob1.json()
     temp.textContent=`${weatherObj.main.temp}°c`
