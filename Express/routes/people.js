d4304aa987fc51f3241751a6db3b1b6de87c6c8c
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-22 09:49:16 +0530

    Update people.js

--- Express/routes/people.js
+++ Express/routes/people.js
@@ -1,16 +1,13 @@
 const express=require('express')
 const router=express.Router()
-let {people}=require('../data')
 
-router.get('/', (req,res)=>{
-    res.status(200).send({success:true, data:people})
-})
-router.post('/', (req,res)=>{
-    const {name}=req.body
-    if(!name) {
-        return res.status(400).json({success:false,msg:"please provide new value"})
-    }
-    res.status(201).send({success:true, person:name})
-})
+const {getPeople, createPerson}=require('../controllers/peopleController')
+
+//this is one way of setting up the routes
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+
+//this is another way of setting the routes
+router.route('/').get(getPeople).post(createPerson)
 
 module.exports=router
