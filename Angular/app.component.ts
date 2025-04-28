f68e5902713fc2c78aad8dfb15e7fa38067de7a2
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-28 17:49:36 +0530

    Add files via upload

---
+++ Angular/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hbm-root',
+  templateUrl: './app.component.html',
+  // template:`<h1>Hello, I'm Abhiram</h1>
+  //           <p>I'm from Hyderabad, Telangana</p>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hotelBookingApplication';
+}
