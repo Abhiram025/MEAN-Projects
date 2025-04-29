4208d347de7830bab4394a5cfd65ee5fc66e00e4
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-29 17:24:38 +0530

    Update rooms.component.ts

--- Angular/rooms.component.ts
+++ Angular/rooms.component.ts
@@ -1,54 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {Room, RoomList} from './rooms'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Component({
-  selector: 'hbm-rooms',
-  templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  selector: 'hbm-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css']
 })
-export class RoomsComponent implements OnInit {
-  hideRooms=false
+export class RoomsListComponent implements OnInit {
+  @Input() room:RoomList[]=[]
+  @Output() selectedRoom=new EventEmitter<RoomList>()
 
-  rooms:Room = {
-    totalRooms: 20,
-    availableRooms: 10,
-    bookedRooms: 4
+  constructor() {}  
+  ngOnInit(): void {
+    
   }
 
-  roomsList:RoomList[]=[
-    {
-      roomNumber: "A-104",
-      roomType:"Deluxe Room",
-      amenities:"Free Wifi, A/C Room, 24/7 Water",
-      price:1200,
-      photo:'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      checkIn:new Date('28-Apr-2025'),
-      checkOut:new Date('29-Apr-2025')
-    },
-    {
-      roomNumber: "C-312",
-      roomType:"Private Suite",
-      amenities:"Free Wifi, A/C Room, 24/7 Water",
-      price:3200,
-      photo:'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      checkIn:new Date('3-May-2025'),
-      checkOut:new Date('5-May-2025')
-    },
-    {
-      roomNumber: "B-705",
-      roomType:"Basic Room",
-      amenities:"Free Wifi, A/C Room, 24/7 Water",
-      price:500,
-      photo:'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      checkIn:new Date('1-May-2025'),
-      checkOut:new Date('2-May-2025')
-    }
-  ]
-  constructor() {}
-
-  ngOnInit():void{}
-
-  toggle() {
-    this.hideRooms=!this.hideRooms
+  selectRoom(room:RoomList) {
+    this.selectedRoom.emit(room)
   }
 }
