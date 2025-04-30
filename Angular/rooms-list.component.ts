95b898ae54c465a9498d5a8a8dc32cd9104132f6
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-04-30 15:56:52 +0530

    Update rooms-list.component.ts

--- Angular/rooms-list.component.ts
+++ Angular/rooms-list.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hbm-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.css']
+  styleUrls: ['./rooms-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
   @Input() room:RoomList[]=[]
+  @Input() title:String=''
   @Output() selectedRoom=new EventEmitter<RoomList>()
 
   constructor() {}  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    if(changes['title']) {
+      this.title=changes['title'].currentValue.toUpperCase()
+    }
+  }
+
   ngOnInit(): void {
     
   }
