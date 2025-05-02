1bb888f48cb3cc0d90815af44dc3f460fba99abd
Abhiram025 <122280936+Abhiram025@users.noreply.github.com>
2025-05-02 17:46:34 +0530

    Update app.component.ts

--- Angular/app.component.ts
+++ Angular/app.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ViewChild, OnInit, 
+         ViewContainerRef, ElementRef} from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 @Component({
   selector: 'hbm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'hotelBookingApplication'
   firstName='Abhiram'
   hotelName='Baratie'
-
+  // role='users'
   
+  @ViewChild('rooms', {read:ViewContainerRef}) view!:ViewContainerRef
+  @ViewChild('name',{static:true}) name!:ElementRef
 
   constructor() {}
-  ngOnInit():void{}  
+  ngOnInit(): void {
+    this.name.nativeElement.innerText=`Checkout the rooms we have Abhi!!`
+  } 
+  ngAfterViewInit(): void {
+    const component=this.view.createComponent(RoomsComponent)
+    component.instance.rooms.availableRooms=20
+  }
 }
